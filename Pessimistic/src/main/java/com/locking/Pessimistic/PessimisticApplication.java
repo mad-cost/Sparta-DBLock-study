f62791ad213a41084bf99775a3a2cbc2c3b9fbbf
mad-cost <mad_scientist1998@naver.com>
2024-08-21 12:11:15 +0900

    pessimistic init commit

---
+++ Pessimistic/src/main/java/com/locking/Pessimistic/PessimisticApplication.java
@@ -0,0 +1,13 @@
+package com.locking.Pessimistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessimisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessimisticApplication.class, args);
+	}
+
+}
---
+++ Pessimistic/src/test/java/com/locking/Pessimistic/PessimisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locking.Pessimistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessimisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
